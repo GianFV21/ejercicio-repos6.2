7281103d6405b839f2877a673c161b91a567c1b4
Flores V�squez Carlos Gianfranco <FV23003@ues.edu.sv>
2024-09-10 17:38:29 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio-repos6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_repos6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double Num1, Num2;
+            Double Suma = 0, Multi = 0, Div = 0;
+            Console.WriteLine("Ingrese el numero 1: ");
+            Num1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el numero 2: ");
+            Num2 = Double.Parse(Console.ReadLine());
+            Suma = Num1 + Num2;
+            Multi = Num1 * Num2;
+            Div = Num1 / Num2;
+            Console.WriteLine("El resultado de la suma es de:{0} ", Suma);
+            Console.WriteLine("El resultado de la Multiplicacion es: {1}", Multi);
+            Console.WriteLine("El resultado de la Multiplicacion es: {2}", Div);
+            Console.ReadKey();
+        }
+    }
+}
